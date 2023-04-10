6ccc3e8f6b724c4b11da95eeffa5950ccac65239
Arthur <arthurzhuk@gmail.com>
2023-04-10 15:59:50 -0700

    current0 indx

--- components/Blockchain.jsx
+++ components/Blockchain.jsx
@@ -170,7 +170,7 @@ const Blockchain = () => {
       ) : (
         <Block key="loading" text="Loading blocks" color="purple" index={0} />
       )}
-      {blockData.length > 0 && blockRefs.current.length > 0
+      {blockData.length > 0 && blockRefs.current.length && blockRefs.current[0]
         ? blockData
             .slice(0, -1)
             .map((block, index) => (
